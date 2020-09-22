8f00eb668cd678fd50036c3e5ca83ad01fbaac25
Zoltan Szor <zltn.szor@gmail.com>
2020-09-22 11:57:03 +0200

    isClose() function added.

--- definitions.h
+++ definitions.h
@@ -1,6 +1,9 @@
 #ifndef DEFINITIONS
 #define DEFINITIONS
 
+//Macros
+#define EPS_ABS = 1e-16;
+
 //Types
 typedef double real_t;
 typedef std::complex <double> complex_t;
@@ -12,5 +15,18 @@ struct LabeledContainer
     unsigned int ID_;
     complex_t value_;
 };
+
+//Functions
+//Test closeness of two float number
+inline bool isClose (real_t a, real_t b)
+{
+    return std::abs (a - b) < EPS_ABS ? true : false;
+}
+
+//Test closeness of two complex float number
+inline bool isClose (complex_t a, complex_t b)
+{
+    return isClose (a.real (), b.real ()) && isClose (a.imag (), b.imag ());
+}
 
 #endif
